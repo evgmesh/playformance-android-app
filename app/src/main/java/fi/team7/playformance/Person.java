2d5374e84f3f40983dd5d2f7a7168c2f13f62be4
Evgenii Meshcheriakov <evgenii.meshcheriakov@metropolia.fi>
2020-11-20 11:17:14 +0200

    New instances and data storage declaration in created classes - Person, Player.

--- app/src/main/java/fi/team7/playformance/Person.java
+++ app/src/main/java/fi/team7/playformance/Person.java
@@ -1,4 +1,28 @@
 package fi.team7.playformance;
 
 public class Person {
+    private final String firstName;
+    private final String lastName;
+    private int number;
+
+    /**
+     * Creates a person with known name and number in a team
+     * @param firstName
+     * @param lastName
+     * @param number
+     */
+
+    public Person(String firstName, String lastName, int number) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.number = number;
+    }
+
+    @Override
+    //Called automatically when listView shows info of players
+    public String toString() {
+        return "Player " + firstName + " " + lastName + ", number " + number;
+    }
+
 }
+
--- app/src/main/java/fi/team7/playformance/Player.java
+++ app/src/main/java/fi/team7/playformance/Player.java
@@ -1,4 +1,35 @@
 package fi.team7.playformance;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Player {
+
+    //Only one instance of this class is created automatically
+    //when the class is loaded
+    private static final Player playerInstance = new Player();
+
+    /**
+     * Get reference to the Players data stories which is a SINGLETON
+     * @return
+     */
+    public static Player getInstance(){
+        return playerInstance;
+    }
+
+    //List guarantees the list to hold original order
+    private List<Player> players;
+
+    private Player(){
+        this.players = new ArrayList<>();
+        // new players can be added here in bellow
+
+    }
+
+    //This will be used by the ListView listing all players into ListView widget
+    public List<Player> getAllPlayers() {
+        return this.players;
+    }
+
+
 }
