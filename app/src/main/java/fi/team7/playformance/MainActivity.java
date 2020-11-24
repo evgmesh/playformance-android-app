6cc1b20ce264650a5293cf8de8d1ec26c27208c3
Evgenii Meshcheriakov <evgenii.meshcheriakov@metropolia.fi>
2020-11-24 11:29:42 +0200

    Creation of second activity. Work on classes Team, Player and Coach

--- app/src/main/java/fi/team7/playformance/MainActivity.java
+++ app/src/main/java/fi/team7/playformance/MainActivity.java
@@ -2,13 +2,22 @@ package fi.team7.playformance;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    private final View.OnClickListener onClickListener = view -> {
+        Intent intent = new Intent(this, SecondActivity.class);
+        startActivity(intent);
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.btn1).setOnClickListener(onClickListener);
     }
 }
--- app/src/main/java/fi/team7/playformance/Team.java
+++ app/src/main/java/fi/team7/playformance/Team.java
@@ -1,4 +1,24 @@
 package fi.team7.playformance;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Team {
+    private Coach coach;
+    private String name;
+    private List<Player> players = new ArrayList<>();
+
+    public Team(Coach coach, String name) {
+        this.coach = coach;
+        this.name = name;
+    }
+    // A method to add Team members
+    // A method to change coach if needed
+    // Getters
+
+
+    public Coach getCoach() {
+        return coach;
+    }
 }
+
