74009350c6fd57c5d843233b4226facae4fbfea2
HTrung <Trung.Nguyen6@metropolia.fi>
2020-11-30 23:35:23 +0700

    Added FaultsSreen, SelectionofPlayer, SelectActionforPlayer, WinsScreen, FaultsSreen
    I dont know how to present Player ID ( box which is placed on the top of screen, in pptx, it is <Placeholder of a player> ) on layout

---
+++ app/src/main/java/fi/team7/playformance/ActivitySelectionForPlayer.java
@@ -0,0 +1,14 @@
+package fi.team7.playformance;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ActivitySelectionForPlayer extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_selection_for_player);
+    }
+}
---
+++ app/src/main/java/fi/team7/playformance/FaultsSreen.java
@@ -0,0 +1,14 @@
+package fi.team7.playformance;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FaultsSreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_faults_sreen);
+    }
+}
