1ddda8939fc7b1aaf06b7fbcc7f9a7dee6e84dc5
HTrung <Trung.Nguyen6@metropolia.fi>
2020-12-01 20:14:12 +0700

    design UI

--- app/src/main/java/fi/team7/playformance/selectexistedteam.java
+++ app/src/main/java/fi/team7/playformance/selectexistedteam.java
@@ -3,6 +3,7 @@ package fi.team7.playformance;
 import android.app.DatePickerDialog;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
 
@@ -10,28 +11,46 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Scanner;
 
 public class selectexistedteam extends AppCompatActivity {
 
     EditText edtDate;
+    int NumberofGame;
+
+    EditText Gamenumber;
+    Button submitButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_selectexistedteam);
 
+        submitButton = (Button) findViewById(R.id.clickConfirm);
+        Gamenumber = (EditText) findViewById(R.id.gameNumber);
         edtDate = (EditText) findViewById(R.id.editTextDate);
         edtDate.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
+            public void onClick(View view) {
                 SelectDate();
+            }
+        });
 
+
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //READ THE VALUE OF GAME NUMBER
+                //READ THE VALUE OF DATE
+                //CREATE AN INTEN AND SEND PARAMETER TO SECOND PAGE
             }
         });
     }
 
 
-    private void    SelectDate() {
+
+
+    private void SelectDate() {
         Calendar calendar = Calendar.getInstance();
         int day = calendar.get(Calendar.DATE);
         int month = calendar.get(Calendar.MONTH);
